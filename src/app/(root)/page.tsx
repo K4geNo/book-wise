9da844fc38756dfece28642cca54fe36cbc947af
K4geNo <micaraujo71@gmail.com>
2023-07-18 19:15:09 -0300

    Chore(component): Created LatestRatings

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,12 @@
 import { LatestRatings } from '@/components/LatestRatings'
+import { PopularBooks } from '@/components/PopularBooks'
 
 export default function Home() {
     return (
         <main className="grid h-full w-full grid-cols-[1fr,308px] gap-x-16 overflow-hidden">
             <LatestRatings />
 
-            <div>
-                <h1>book</h1>
-            </div>
+            <PopularBooks />
         </main>
     )
 }
