58d2ecbf826e3a6aec7d08df3babd897065dc5dd
K4geNo <micaraujo71@gmail.com>
2023-07-20 17:03:07 -0300

    Chore: added axios and react query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,13 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { ReactNode } from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 export function Providers({ children }: { children: ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SessionProvider>{children}</SessionProvider>
+        </QueryClientProvider>
+    )
 }
