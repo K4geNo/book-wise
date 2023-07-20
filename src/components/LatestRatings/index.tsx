5d9c73c72efb3aff2b357dec0b57f653b7a29c9e
K4geNo <micaraujo71@gmail.com>
2023-07-20 17:05:17 -0300

    Chore(Fetch): Fetch done on components

--- src/components/LatestRatings/index.tsx
+++ src/components/LatestRatings/index.tsx
@@ -1,9 +1,22 @@
+'use client'
+
 import { PiChartLineUp } from 'react-icons/pi'
 import { PageTitle } from '../PageTitle'
 import { Text } from '../Typography'
-import { RatingCard } from '../RatingCard'
+import { RatingCard, RatingWithAuthorAndBook } from '../RatingCard'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/lib/axios'
 
 export const LatestRatings = () => {
+    const { data: ratings } = useQuery<RatingWithAuthorAndBook[]>(
+        ['latest-ratings'],
+        async () => {
+            const { data } = await api.get('/ratings/latest')
+
+            return data?.ratings ?? []
+        },
+    )
+
     return (
         <div className="scrollbar-hidden flex h-full w-full flex-col overflow-y-auto">
             <PageTitle icon={PiChartLineUp} title="Início" className="mb-10" />
@@ -11,37 +24,8 @@ export const LatestRatings = () => {
             <Text size="sm">Avaliações mais recentes</Text>
 
             <section className="mt-4 flex flex-col gap-y-3">
-                {Array.from({ length: 20 }).map((_, index) => (
-                    <RatingCard
-                        key={index}
-                        rating={{
-                            id: 'aa',
-                            rate: 4,
-                            user: {
-                                name: 'Lucas',
-                                avatar_url:
-                                    'https://avatars.githubusercontent.com/u/4256471?v=4',
-                                email: 'johndoe@example.com',
-                                id: 'daisdja',
-                                created_at: new Date(),
-                            },
-                            book: {
-                                id: 'daisdja',
-                                author: 'Lucas',
-                                name: 'Lucas',
-                                cover_url:
-                                    'https://avatars.githubusercontent.com/u/4256471?v=4',
-                                created_at: new Date(),
-                                summary:
-                                    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum nisi laborum consequatur similique in quaerat a, aliquam vero accusamus, quasi hic commodi assumenda eveniet cum perspiciatis blanditiis! Dolore id ipsa blanditiis velit qui earum veniam corporis natus vitae ratione fuga quis est dolorum voluptatum provident, eligendi deleniti explicabo voluptatibus possimus. Pariatur quisquam provident perspiciatis dolore sint maiores delectus officiis quis hic? Ea hic ut itaque molestias et magni cupiditate veniam qui, dolore, possimus asperiores architecto, praesentium beatae nam consequuntur voluptatem? Deserunt, ipsa? Reiciendis ut laudantium atque, laboriosam quam a natus ducimus id cum ad animi impedit magnam pariatur modi, assumenda consequuntur ipsum? Nostrum unde nam eius facilis dolorum dolores dolorem amet eligendi, accusantium rem recusandae ea, explicabo assumenda, eos pariatur sint excepturi dignissimos! Illum totam quam sint tenetur eligendi. Enim ad, aut, suscipit hic itaque officia cupiditate, nam voluptatem id ducimus perferendis illo saepe asperiores similique accusamus? Rerum quis, aspernatur amet neque ex laboriosam minus reiciendis exercitationem iste dolore! Aliquid molestias modi numquam eligendi voluptas assumenda tenetur, culpa perspiciatis voluptatum adipisci asperiores beatae ea doloribus voluptate ducimus ullam libero. Sapiente est quis officiis. Eaque accusantium unde est esse sint, provident corrupti neque quas minima pariatur nisi earum consequuntur saepe minus.',
-                                total_pages: 100,
-                            },
-                            created_at: new Date(),
-                            book_id: 'daisdja',
-                            user_id: 'daisdja',
-                            description: 'Lucas',
-                        }}
-                    />
+                {ratings?.map((rating) => (
+                    <RatingCard key={rating.id} rating={rating} />
                 ))}
             </section>
         </div>
--- src/components/PopularBooks/index.tsx
+++ src/components/PopularBooks/index.tsx
@@ -1,34 +1,33 @@
-import { BookCard } from '../BookCard'
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { BookCard, BookWithAvgRating } from '../BookCard'
 import { Text } from '../Typography'
 import { Link } from '../ui/link'
+import { api } from '@/lib/axios'
 
 // interface PopularBooksProps {}
 
 export const PopularBooks = () => {
+    const { data: popularBooks } = useQuery<BookWithAvgRating[]>(
+        ['popular-books'],
+        async () => {
+            const { data } = await api.get('/books/popular')
+
+            return data?.books ?? []
+        },
+    )
+
     return (
-        <div className="mt-10 flex w-full flex-col gap-4">
+        <div className="mt-14 flex w-full flex-col gap-4">
             <header className="flex items-center justify-between">
                 <Text size="sm">Livros populares</Text>
                 <Link href="/explore" text="Ver todos" />
             </header>
 
             <section className="flex flex-col gap-3">
-                {Array.from({ length: 4 }).map((_, i) => (
-                    <BookCard
-                        key={`popular-${i}`}
-                        book={{
-                            author: 'J. K. Rowling',
-                            avgRating: 4,
-                            cover_url:
-                                'https://m.media-amazon.com/images/I/51UoqRAxwEL.jpg',
-                            created_at: new Date(),
-                            id: '1',
-                            name: 'Harry Potter e a Pedra Filosofal',
-                            summary:
-                                'Harry Potter e a Pedra Filosofal é o primeiro livro dos sete volumes da série de fantasia Harry Potter, tanto em termos cronológicos como em ordem de publicação, da autora inglesa J. K. Rowling.',
-                            total_pages: 223,
-                        }}
-                    />
+                {popularBooks?.map((book) => (
+                    <BookCard key={book.id} book={book} />
                 ))}
             </section>
         </div>
